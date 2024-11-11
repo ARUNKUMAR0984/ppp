bef388596d0e673cc605a1bfa7eadb769dcf5f1e
ARUNKUMAR0984 <115545171+ARUNKUMAR0984@users.noreply.github.com>
2024-11-12 02:34:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,43 +7,41 @@ import cv2
 from keras.models import load_model
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-app = Flask(__name__)
+from tensorflow.keras.preprocessing.image import img_to_array
+from io import BytesIO
 
-model =load_model("model.h5")
+app = Flask(_name_)
+
+model = load_model("D:\\PLANT_Disease\\plant_disease_detection-main\\plant_disease_detection-main\\model.h5")
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 labels = {0: 'Healthy', 1: 'Powdery', 2: 'Rust'}
 
-
-def getResult(image_path):
-    img = load_img(image_path, target_size=(225,225))
+def getResult(img):
+    img = img.resize((225, 225))  # Resize image to target size
     x = img_to_array(img)
     x = x.astype('float32') / 255.
     x = np.expand_dims(x, axis=0)
     predictions = model.predict(x)[0]
     return predictions
 
-
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
-
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         f = request.files['file']
-
-        basepath = os.path.dirname(_file_)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
-        predictions=getResult(file_path)
+        
+        # Load image directly from memory
+        img = Image.open(BytesIO(f.read()))
+        
+        predictions = getResult(img)
         predicted_label = labels[np.argmax(predictions)]
+        
         return str(predicted_label)
     return None
 
-
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(debug=True)
